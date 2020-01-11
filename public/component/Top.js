97eb4ed3dfc865c9385b76fc4cdd51754d1466be
qiuguanglin <tmdyyzc$123>
2020-01-11 13:06:04 +0800

    ajusted the header fonts

--- public/component/Top.js
+++ public/component/Top.js
@@ -10,11 +10,12 @@ class TopPanel extends Component{
     return(
       <div id="topPanel">
         <h1>
-          <span id="top-logo1"><FormattedMessage id="top-logo1"/></span>
-          <span id="top-logo2"><FormattedMessage id="top-logo2"/></span>
+          <span><FormattedMessage id="top-logo1"/></span>
+          <span><FormattedMessage id="top-logo2"/></span>
         </h1>
         <h2>
-          <FormattedMessage id="top-h2"/>
+          <span><FormattedMessage id="top-h2-left"/></span>
+          <span><FormattedMessage id="top-h2-right"/></span>
         </h2>
         <h3>
           <img src="../resource/cosmos.png"/>
