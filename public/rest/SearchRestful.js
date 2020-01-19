9dad62b9751c9a52b8191fb811597e820d010cde
qiuguanglin <tmdyyzc$123>
2020-01-19 15:41:01 +0800

    add error message when there is a searching exception

--- public/rest/SearchRestful.js
+++ public/rest/SearchRestful.js
@@ -9,7 +9,7 @@ const Search = (from, to, flightsMap, planetList, routeCostMap, callback) =>{
   RestRequester.get(`${searchResource}/flights/${from}/${to}`)
   .then(res => {
     if(!res.data.success)
-      throw Error(res.data.message);
+      throw Error('error when searching routes', from, to, res.data.message);
     return res;
   })
   .then(res => Adapter(res, flightsMap, planetList, routeCostMap))
